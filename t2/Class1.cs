9e0b7b94dec11642edec5279619de87d50e4a0f4
LINCHENGCHI <d11416112@gs.takming.edu.tw>
2026-04-28 12:06:16 +0800

    Update Class1.cs

--- t2/Class1.cs
+++ t2/Class1.cs
@@ -1,30 +1,21 @@
-﻿using System;
-using System.Security.Cryptography;
-
 public partial class Test
 {
-	private int Mid;
-    private int Final;
+    int Mid, Final;
 
-    public void SetGrade(int Mid,int Final)
+    public void SetGrade(int Mid, int Final)
     {
-        if(ValidGrade(Mid, Final))
+        if (ValidGrade(Mid, Final))
         {
             this.Mid = Mid;
             this.Final = Final;
         }
     }
 
-    public double GetAvg()
-    {
-        return (Mid + Final) / 2.0;
-    }
+    public double GetAvg() => (Mid + Final) / 2.0;
 }
 
 public partial class Test
 {
-    private bool ValidGrade(int Mid, int Final)
-    {
-        return (Mid >= 0 && Mid <= 100 && Final >= 0 && Final <= 100);
-    }
+    bool ValidGrade(int Mid, int Final)
+        => Mid >= 0 && Mid <= 100 && Final >= 0 && Final <= 100;
 }
